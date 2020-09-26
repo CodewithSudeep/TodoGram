2120c50e4e0da1cbf7cc526b8f807ede32b2646c
sangya2001 <sangyasherpa2058@gmail.com>
2020-09-26 20:40:38 +0545

    Home Layout completed

--- README.md
+++ README.md
@@ -5,8 +5,4 @@ Content
 
 ```
 npm install --save express nodemon
-<<<<<<< HEAD
 ```
-=======
-```
->>>>>>> f7251b8799d52997381168223645f31dd691302d
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import Button from "@material-ui/core/Button";
+import NoMeetingRoomIcon from "@material-ui/icons/NoMeetingRoom";
 
-export default function Home({logout}) {
-    const expireSession = () => logout();
+export default function Home({ logout }) {
+  const expireSession = () => logout();
 
-    return (
-        <div>
-            This is home
-            <button onClick={() => expireSession()}>
-                logout
-            </button>
-        </div>
-    )
+  return (
+    <div className="home">
+      {/* title */}
+      <div className="home-title">TodoGram</div>
+
+      <div className="home-tabs">
+        {/* left card screen  */}
+        <div className="home-tabsLeft home-tabCard">LeftTab</div>
+
+        {/* right card screen */}
+        <div className="home-tabsRight home-tabCard">RIghtTab</div>
+      </div>
+
+      {/* footer */}
+      <div className="home-footer">
+        <Button onClick={() => expireSession()}>
+          <NoMeetingRoomIcon /> Logout
+        </Button>
+        <div>Copyright &copy; CodewithSudeep 2020. All Rights Reserved</div>
+      </div>
+    </div>
+  );
 }
